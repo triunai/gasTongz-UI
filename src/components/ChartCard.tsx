faec340ff4b10546b86d6b60da33ee74da565661
Khumeren <khucode@gmail.com>
2025-02-14 23:42:34 +0800

    fix: fixed chart styling being ugly

--- src/components/ChartCard.tsx
+++ src/components/ChartCard.tsx
@@ -7,7 +7,7 @@ interface ChartCardProps {
 }
 
 const ChartCard: React.FC<ChartCardProps> = ({ title, chart }) => (
-  <div className="bg-gray-850 text-white p-6 rounded-lg shadow-lg-amber-600 border-amber-800">
+  <div className="bg-black text-white p-6 rounded-lg shadow-lg shadow-amber-500 border-amber-800">
     <h3 className="text-xl font-semibold mb-4 border-amber-800">{title}</h3>
     <div className="chart-container bg-black rounded-lg p-4">
       {chart}
